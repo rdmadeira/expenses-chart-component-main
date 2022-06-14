7c85883f15dc61876a3aa91a67d03f652239e4b4
rdmadeira <rdmadeira2@gmail.com>
2022-06-14 14:21:01 -0300

    solucion de addeventlistener con uso del LET en el bucle forgit add . !

--- main.js
+++ main.js
@@ -37,34 +37,34 @@ function amountAsign(){
     barrasVerticales[hoy - 1].style.backgroundColor = 'var(--Cyan)';
     barrasVerticales[hoy - 1].addEventListener('mouseover',()=> barrasVerticales[hoy - 1].style.backgroundColor = 'hsla(186, 34%, 60%, 0.59)');
     barrasVerticales[hoy - 1].addEventListener('mouseout',()=> barrasVerticales[hoy - 1].style.backgroundColor = 'var(--Cyan');
-    for(i=0, j=0; i<barrasVerticales.length; i++, j++) {
+    for(let i=0; i<barrasVerticales.length; i++) {
         barrasVerticales[i].style.height = dataAmount[i].amount * 3 + 'px';
         barrasVerticales[i].setAttribute('id', 'bv' + i);
-        // barrasVerticales[i].addEventListener('mouseover', ()=> appearValue(j));  --- No funcionó en el bucle
-        // barrasVerticales[i].addEventListener('mouseout', ()=> // disappearValue());    --- No funcionó en el bucle
+        barrasVerticales[i].addEventListener('mouseover', ()=> appearValue(i));
+        barrasVerticales[i].addEventListener('mouseout', disappearValue);
     }
-    barrasVerticales[0].addEventListener('mouseover', ()=> appearValue(0));
-    barrasVerticales[0].addEventListener('mouseout', ()=> disappearValue(0));
-    barrasVerticales[1].addEventListener('mouseover', ()=> appearValue(1));
-    barrasVerticales[1].addEventListener('mouseout', ()=> disappearValue(1));
-    barrasVerticales[2].addEventListener('mouseover', ()=> appearValue(2));
-    barrasVerticales[2].addEventListener('mouseout', ()=> disappearValue(2));
-    barrasVerticales[3].addEventListener('mouseover', ()=> appearValue(3));
-    barrasVerticales[3].addEventListener('mouseout', ()=> disappearValue(3));
-    barrasVerticales[4].addEventListener('mouseover', ()=> appearValue(4));
-    barrasVerticales[4].addEventListener('mouseout', ()=> disappearValue(4));
-    barrasVerticales[5].addEventListener('mouseover', ()=> appearValue(5));
-    barrasVerticales[5].addEventListener('mouseout', ()=> disappearValue(5));
-    barrasVerticales[6].addEventListener('mouseover', ()=> appearValue(6));
-    barrasVerticales[6].addEventListener('mouseout', ()=> disappearValue(6));
+    //barrasVerticales[0].addEventListener('mouseover', ()=> appearValue(0));
+    //// barrasVerticales[0].addEventListener('mouseout', ()=> disappearValue(0));
+    //barrasVerticales[1].addEventListener('mouseover', ()=> appearValue(1));
+    //// barrasVerticales[1].addEventListener('mouseout', ()=> disappearValue(1));
+    //barrasVerticales[2].addEventListener('mouseover', ()=> appearValue(2));
+    //// barrasVerticales[2].addEventListener('mouseout', ()=> disappearValue(2));
+    //barrasVerticales[3].addEventListener('mouseover', ()=> appearValue(3));
+    //// barrasVerticales[3].addEventListener('mouseout', ()=> disappearValue(3));
+    //barrasVerticales[4].addEventListener('mouseover', ()=> appearValue(4));
+    //// barrasVerticales[4].addEventListener('mouseout', ()=> disappearValue(4));
+    //barrasVerticales[5].addEventListener('mouseover', ()=> appearValue(5));
+    //// barrasVerticales[5].addEventListener('mouseout', ()=> disappearValue(5));
+    //barrasVerticales[6].addEventListener('mouseover', ()=> appearValue(6));
+    //// barrasVerticales[6].addEventListener('mouseout', ()=> disappearValue(6));
     
     function appearValue(el) {
          console.log(dataAmount[el].amount)
-         style.insertRule(`#bv${el}::before {content: "${dataAmount[el].amount}"; background-color: var(--Dark_brown); color: var(--Very_pale_orange); font-size: 0.5em; font-weight: 700; position: absolute; top: -30px; left: -10px; padding: 5px 10px; border-radius: 5px;}`, style.cssRules.length);
+         style.insertRule(`#bv${el}::before {content: "${dataAmount[el].amount}";  background-color: var(--Dark_brown); color: var(--Very_pale_orange);  font-size: 0.5em; font-weight: 700; position: absolute; top: -30px;  left: -10px; padding: 5px 10px; border-radius: 5px;}`,  style.cssRules.length);
     }
     function disappearValue() {
         style.deleteRule(style.cssRules.length -1);
-   }
-   console.log(style);
+    }
+    console.log(style);
 }
 amountAsign();
