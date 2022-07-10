b7e7d80773abf010906f1e54d012eb0797fa4c76
rdmadeira <rdmadeira2@gmail.com>
2022-07-10 18:58:52 -0300

    Se usó fetch para capturar data de data.json

--- main.js
+++ main.js
@@ -1,48 +1,33 @@
-function amountAsign(){
-    let dataAmount = [
-        {
-        "day": "mon",
-        "amount": 17.45
-        },
-        {
-        "day": "tue",
-        "amount": 34.91
-        },
-        {
-        "day": "wed",
-        "amount": 52.36
-        },
-        {
-        "day": "thu",
-        "amount": 31.07
-        },
-        {
-        "day": "fri",
-        "amount": 23.39
-        },
-        {
-        "day": "sat",
-        "amount": 43.28
-        },
-        {
-        "day": "sun",
-        "amount": 25.48
-        }
-    ]
+async function amountAsign(){
+    let dataAmount = fetch('./data.json');
+    dataAmount.then(res=>{return res.json()})
+              .then(data=> {bucleAsign(data);})
+              .catch(err=>console.error(`${err}`));  
     let fecha = Date.now(); // genera un numero
     let hoy = new Date(fecha).getDay(); // genera numero de 0 a 6
     hoy == 0 ? hoy = 7 : hoy;
     let style = document.styleSheets.item(0);
     const barrasVerticales = document.getElementsByClassName('barra_vertical');
     barrasVerticales[hoy - 1].classList.replace('soft-red', 'cyan');
-    
-    for(let i=0; i<barrasVerticales.length; i++) {    
-        barrasVerticales[i].style.height = dataAmount[i].amount * 3 + 'px';
-        barrasVerticales[i].setAttribute('id', 'bv' + i);
+    /* let dataAmount1 = await dataAmount;
+    console.log(dataAmount1); */
+    function bucleAsign(data){
+        for(let i=0; i<barrasVerticales.length; i++) {   
+            barrasVerticales[i].style.height = 
+                data[i].amount * 3 + 'px';
+            barrasVerticales[i].setAttribute('id', 'bv' + i);
 
-        // funcionó declarando let al i, para que se mantenga en el scopo de la funcion.
-        barrasVerticales[i].addEventListener('mouseover', ()=> appearValue(i));
-        barrasVerticales[i].addEventListener('mouseout', disappearValue);
+            // funcionó declarando let al i, para que se mantenga en el scopo de la funcion.
+            barrasVerticales[i].addEventListener('mouseover', ()=> appearValue(i));
+            barrasVerticales[i].addEventListener('mouseout', disappearValue);
+        }
+        function appearValue(el) {
+            console.log(data[el].amount);
+            style.insertRule(`#bv${el}::before {content: "${data[el].amount}";  background-color: var(--Dark_brown); color: var(--Very_pale_orange);  font-size: 0.5em; font-weight: 700; position: absolute; top: -30px;  left: -10px; padding: 5px 10px; border-radius: 5px;}`,  style.cssRules.length);
+        }
+        function disappearValue() {
+            style.deleteRule(style.cssRules.length -1);
+        }
     }
     //barrasVerticales[0].addEventListener('mouseover', ()=> appearValue(0));
     //// barrasVerticales[0].addEventListener('mouseout', ()=> disappearValue(0));
@@ -59,13 +44,7 @@ function amountAsign(){
     //barrasVerticales[6].addEventListener('mouseover', ()=> appearValue(6));
     //// barrasVerticales[6].addEventListener('mouseout', ()=> disappearValue(6));
     
-    function appearValue(el) {
-         console.log(dataAmount[el].amount)
-         style.insertRule(`#bv${el}::before {content: "${dataAmount[el].amount}";  background-color: var(--Dark_brown); color: var(--Very_pale_orange);  font-size: 0.5em; font-weight: 700; position: absolute; top: -30px;  left: -10px; padding: 5px 10px; border-radius: 5px;}`,  style.cssRules.length);
-    }
-    function disappearValue() {
-        style.deleteRule(style.cssRules.length -1);
-    }
+    
     console.log(style);
 }
 amountAsign();
